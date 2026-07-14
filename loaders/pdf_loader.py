f5ad114afa79ccef8a795c468549a19456a2a486
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-07-14 23:58:47 +0530

    implementing pdf loader

--- loaders/pdf_loader.py
+++ loaders/pdf_loader.py
@@ -1,8 +1,19 @@
-from langchain_docling.loader import DoclingLoader
+from langchain_docling.loader import DoclingLoader # type: ignore
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+class PdfLoader:
+    def pdf_loader(self, file_path: str):
+        loader = DoclingLoader(file_path=file_path)        
+        document = loader.load()
+        loader.info(f"==========> {len(document)}")
+        return document
 
 
 
-
-
-
-loader = DoclingLoader
