19f792d62863411adc778ba408ed273bdf771be7
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-07-16 22:07:08 +0530

    implementing text splitter

--- loaders/pdf_loader.py
+++ loaders/pdf_loader.py
@@ -4,8 +4,8 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    filename='app.log',
-    filemode='w',
+    # filename='app.log',
+    # filemode='w',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
--- splitters/splitter.py
+++ splitters/splitter.py
@@ -1,19 +1,21 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from lang
+from langchain_core.documents import Document
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class Splitter:
-    def chunking(self, docs_to_split:list)->list:
+    def split(self, documents: list[Document]) -> list[Document]:
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size = 500,
             chunk_overlap = 100,
             separators=["\n\n", "\n", " ", ""], 
             add_start_index=True,
         )
-        docs = text_splitter.split_documents(docs_to_split)
+        docs = text_splitter.split_documents(documents)
         
-        
+        logger.info("Created %d chunks", len(docs))
+
+        return docs
         ...
