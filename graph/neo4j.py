1c8446e777e4365439cad2f1c244a16fada16592
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-07-25 23:54:21 +0530

    implementing neo4j

--- graph/neo4j.py
+++ graph/neo4j.py
@@ -1,1 +1,21 @@
 from langchain_community.graphs import Neo4jGraph
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Neo4jStorage():
+    def __init__(self):
+        url = os.getenv("NEO4J_URI")
+        username = os.getenv("NEO4J_USERNAME")
+        password = os.getenv("NEO4J_PASSWORD")
+        database = os.getenv("NEO4J_DATABASE")
+        instance = os.getenv("AURA_INSTANCEID")
+        instance_name = os.getenv("AURA_INSTANCENAME")
+        
+        self.Neo4jGraph(
+            url = url,
+            username = username,
+            password = password
+        )
+        pass
