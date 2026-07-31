33ccce916faa6ed29feda08a0d0b1fa0e97966ca
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-07-31 22:18:39 +0530

    vector retriever

--- retrieval/vector_retriever.py
+++ retrieval/vector_retriever.py
@@ -1,1 +1,18 @@
 from embeddings.embedding import Embeddings 
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+
+
+
+
+load_dotenv()
+
+
+class VectorRetrievr():
+    pass
+
+
+
+
+
