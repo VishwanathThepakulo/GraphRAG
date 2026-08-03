46832a3f50bfb8b4d68adfe381c97b22cc573e86
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-08-03 23:50:12 +0530

    implementing retriever

--- retrieval/vector_retriever.py
+++ retrieval/vector_retriever.py
@@ -2,17 +2,31 @@ from embeddings.embedding import Embeddings
 from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
-
+from pymongo.errors import ServerSelectionTimeoutError, PyMongoError
+import logging
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 
 
 
 load_dotenv()
-
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 class VectorRetrievr():
-    ...
-
-
+    def __init__(self):    
+        connection_string = os.getenv("DB_CREDENTIALS")
+        if not connection_string:
+            raise ValueError("Connection string not found")
+        self.client = MongoClient(connection_string, serverSelectionTimeoutMS=2000)
+        self.collection = self.client['RAG_Collection']['Searchable_TABLE']
+        self.embedding_model = HuggingFaceEndpointEmbeddings(
+            model="sentence-transformers/all-MiniLM-L6-v2",
+            huggingfacehub_api_token=embedding_api_key,
+        )
+        self.reranker = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2')
 
 
 
